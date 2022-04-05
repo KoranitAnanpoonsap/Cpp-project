cdf92a588c5c2b40bfc9627c75ba43f80335ef56
KoranitAnanpoonsap <koranit.bm@gmail.com>
2022-04-05 11:03:03 +0700

    update

--- project.cpp
+++ project.cpp
@@ -5,9 +5,9 @@ int main()
 {
     cout << "Hello, World" << endl;
     cout << "Hello, Bm";
-    cout << "Hello world!!"
-    cout << "\nFinal Project-Object Oriented Programming\n\nC++ OOP financial tool."
-    cout << "\nPresent date : 27 April. 5 minutes each present, 5 minutes for Q/A."
+    cout << "Hello world!!";
+    cout << "\nFinal Project-Object Oriented Programming\n\nC++ OOP financial tool.";
+    cout << "\nPresent date : 27 April. 5 minutes each present, 5 minutes for Q/A.";
     cout << "\n\nGroup Members: \n1. Koranit Ananpoopsap_64011441 \n2. Jirapat Wonjaroenrat_64011405 \n3. Chonlaphat Ink..._" << endl;
     cout << "Test commit";
 }
