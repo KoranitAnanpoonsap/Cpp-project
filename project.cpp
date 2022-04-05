b436be546414b96c1428cb8a2617f0bc9c6b785f
KoranitAnanpoonsap <koranit.bm@gmail.com>
2022-04-05 10:58:09 +0700

    Co-authored-by: Jirapat Wongjaroenrat <Jirapat330@users.noreply.github.com>

--- project.cpp
+++ project.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
+    cout << "Hello, World" << endl;
     cout << "Hello, Bm";
 }
     cout << "Hello world!!"
          << "\nFinal Project-Object Oriented Programming\n\nC++ OOP financial tool."
          << "\nPresent date : 27 April. 5 minutes each present, 5 minutes for Q/A."
          << "\n\nGroup Members: \n1. Koranit Ananpoopsap_64011441 \n2. Jirapat Wonjaroenrat_64011405 \n3. Chonlaphat Ink..._" << endl;
+         << "What's sup";
 }
