c6ea8225934af3a4fafcec98d4265f6558871591
Jirapat Wongjaroenrat <92516335+Jirapat330@users.noreply.github.com>
2022-04-05 10:09:07 +0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, world and Bm";
+    cout << "Hello world!!"
+         << "\nFinal Project-Object Oriented Programming\n\nC++ OOP financial tool."
+         << "\nPresent date : 27 April. 5 minutes each present, 5 minutes for Q/A."
+         << "\n\nGroup Members: \n1. Koranit Ananpoopsap_64011441 \n2. Jirapat Wonjaroenrat_64011405 \n3. Chonlaphat Ink..._" << endl;
 }
