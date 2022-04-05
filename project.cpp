8bece3f1ef3052f212aae8006a791e676d9592f6
Jirapat Wongjaroenrat <92516335+Jirapat330@users.noreply.github.com>
2022-04-05 10:51:14 +0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -9,4 +9,5 @@ int main()
          << "\nFinal Project-Object Oriented Programming\n\nC++ OOP financial tool."
          << "\nPresent date : 27 April. 5 minutes each present, 5 minutes for Q/A."
          << "\n\nGroup Members: \n1. Koranit Ananpoopsap_64011441 \n2. Jirapat Wonjaroenrat_64011405 \n3. Chonlaphat Ink..._" << endl;
+         
 }
